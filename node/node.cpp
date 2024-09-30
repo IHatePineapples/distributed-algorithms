fcb4662439f61f88c8d73ad37ab443496cba6673
Pineapples <91441379+IHatePineapples@users.noreply.github.com>
2024-09-30 01:35:58 +0100

    Add base node.hpp+cpp

---
+++ node/node.cpp
@@ -0,0 +1,25 @@
+#include "node.hpp"
+
+#include <chrono>
+#include <iostream>
+#include <ostream>
+#include <thread>
+
+namespace da {
+
+node::node(const char c, const std::vector<const node *> &n)
+    : name_(c), neighs_(n) {
+  std::cout << c << ": Created." << std::endl;
+}
+
+void node::recv(const node *n) {
+  std::this_thread::sleep_for(std::chrono::seconds{5});
+  fifo_.emplace(n);
+  std::cout << name_ << ": Received <tok> from " << n->name() << std::endl;
+}
+
+void node::send(node *n) const {
+  std::cout << name_ << ": Sent <tok> to " << n->name() << std::endl;
+  n->recv(this);
+};
+} // namespace da
---
+++ node/node.hpp
@@ -0,0 +1,23 @@
+#include <queue>
+#include <vector>
+
+namespace da {
+
+class node {
+  const char name_ = '?';
+  std::vector<const node *> neighs_ = {};
+  std::queue<const node *> fifo_ = {};
+
+public:
+  explicit node(const char c, const std::vector<const node *> &n = {});
+
+  auto &neighs() { return neighs_; }
+  const auto name() const { return name_; }
+
+  virtual void exec() = delete;
+
+  void send(node *n) const;
+  void recv(const node *n);
+  void connect(const node *n);
+};
+} // namespace da
